1825f50dc5c401fbf26745c6f5d0ef94d8ea26e6
thainguyen253 <111984224+thainguyen253@users.noreply.github.com>
2022-09-02 21:39:06 +0700

    Add files via upload

---
+++ SoPhuc/src/SoPhuc.java
@@ -0,0 +1,15 @@
+public class SoPhuc
+{
+    double phanThuc, phanAo;
+    SoPhuc(){};
+
+    SoPhuc(float pt,float pa){
+        this.phanThuc=pt;
+        this.phanAo=pa;
+    }
+    void nhapSoPhuc(){
+        System.out.print("Nhap phan thuc: ");
+        phanThuc=
+    }
+}
+
